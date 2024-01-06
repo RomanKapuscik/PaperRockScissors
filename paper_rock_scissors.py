b8fe6f798ddade5dd61e9c8ec6564f37c29fd7f6
Roman Kapuścik <70838513+RomanKapuscik@users.noreply.github.com>
2024-01-06 14:58:16 +0100

    Update paper_rock_scissors.py

--- paper_rock_scissors.py
+++ paper_rock_scissors.py
@@ -1,43 +1,47 @@
 import random
 import json
+from typing import Tuple
 
 options = ('paper', 'rock', 'scissors')
 
 answer_to_win = {'paper': 'scissors',
-                 'rock':'paper',
-                 'scissors':'rock'}
+                 'rock': 'paper',
+                 'scissors': 'rock'}
 
-#Players choices loaded form file
+# Players choices loaded form file
 with open('results.json') as f:
     results = json.load(f)
 
-rounning = True
+running = True
 score = 0
 number_of_games = 0
-min_number_of_games = 10 #befor riching minimum number of games app will chose answers randomly
+min_number_of_games = 10    # Before ricing minimum number of games, the app will choose answers randomly
 last1, last2 = '', ''
 
 
-def game_result(player: str, computer: str) -> int:
-    if player == computer:
+def game_result(player_choice: str, computers_choice: str) -> Tuple[int, str]:
+    if player_choice == computers_choice:
         return 0, 'tide'
-    elif answer_to_win[player] == computer:
+    elif answer_to_win[player_choice] == computers_choice:
         return -1, 'lose'
     else:
         return 1, 'win'
-    
+
+
 def select_answer(player_answer: str) -> str:
     disp_results = results[player_answer]
     return max(disp_results, key=disp_results.get)
 
-def calculate_number_of_games(results: dict) -> int:
+
+def calculate_number_of_games(game_results: dict) -> int:
     num = 0
-    for keys in results:
-        for key in results[keys]:
-            num += results[keys][key]
+    for keys in game_results:
+        for key in game_results[keys]:
+            num += game_results[keys][key]
     return num
 
-while rounning:
+
+while running:
     player = None
     
     while player not in options:
@@ -56,17 +60,17 @@ while rounning:
     score += result[0]
     number_of_games += 1
     
-    #Update choices of the player
+# Update the choices of the player
     last1, last2 = player, last1
     if number_of_games > 1:
         results[last2][last1] += 1
     
     if input('Do You want to play again? (y/n)').lower() == 'n':
-        rounning = False
+        running = False
         
-print(f'Thanks for playting, You score is {score} points.')
+print(f'Thanks for playing, You score is {score} points.')
 
-#Save results to file
+# Save results to file
 json_object = json.dumps(results, indent=4)
 
 with open('results.json', 'w') as outfile:
